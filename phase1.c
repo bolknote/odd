7f38da90d02c7dfed9438bc0e3c8313e9afa626b
Evgeny Stepanischev <imbolk@gmail.com>
2024-12-21 16:11:59 +0300

    tab → spaces

--- phase1.c
+++ phase1.c
@@ -19,12 +19,12 @@ size_t stack_size = 0;
 size_t stack_capacity = 0;
 
 uint128_t is_divided_by(uint128_t v, uint_fast8_t by) {
-	uint128_t result = v / by;
-	return result * by == v ? result : 0;
+    uint128_t result = v / by;
+    return result * by == v ? result : 0;
 }
 
 void printf_u128(const uint128_t v) {
-	uint64_t low, high;
+    uint64_t low, high;
 
     bool is_little_endian = __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__;
 
@@ -71,31 +71,31 @@ void realloc_stack(uint128_t **stack, size_t *capacity) {
 }
 
 bool check_exists_and_add(const uint128_t v) {
-	size_t pos = 0;
+    size_t pos = 0;
 
-	if (odds_size) {
-	    pos = binary_search_insert_position(odds, odds_size, v);
+    if (odds_size) {
+        pos = binary_search_insert_position(odds, odds_size, v);
 
-	    if (pos < odds_size && odds[pos] == v) {
-	        return true;
-	    }
-	}
+        if (pos < odds_size && odds[pos] == v) {
+            return true;
+        }
+    }
 
     if (odds_size == odds_capacity) {
-    	realloc_stack(&odds, &odds_capacity);
-	}
+        realloc_stack(&odds, &odds_capacity);
+    }
 
-	memmove(&odds[pos + 1], &odds[pos], (odds_size - pos) * sizeof(uint128_t));
+    memmove(&odds[pos + 1], &odds[pos], (odds_size - pos) * sizeof(uint128_t));
 
-	odds[pos] = v;
-	odds_size++;
+    odds[pos] = v;
+    odds_size++;
 
-	return false;
+    return false;
 }
 
 void push_stack(uint128_t value) {
     if (stack_size == stack_capacity) {
-    	realloc_stack(&stack, &stack_capacity);
+        realloc_stack(&stack, &stack_capacity);
     }
 
     stack[stack_size++] = value;
@@ -106,49 +106,49 @@ uint128_t pop_stack() {
 }
 
 void main_enumeration(uint128_t counter) {
-	while (counter != (uint128_t) ~0) {
-		if (check_exists_and_add(counter)) {
-			return;
-		}
+    while (counter != (uint128_t) ~0) {
+        if (check_exists_and_add(counter)) {
+            return;
+        }
 
-		printf_u128(counter);
+        printf_u128(counter);
 
-		uint128_t cnt_by_3 = is_divided_by(counter, 3);
+        uint128_t cnt_by_3 = is_divided_by(counter, 3);
 
-		if (cnt_by_3) {
-			do {
-				if (check_exists_and_add(cnt_by_3)) {
-					break;
-				}
+        if (cnt_by_3) {
+            do {
+                if (check_exists_and_add(cnt_by_3)) {
+                    break;
+                }
 
-				printf_u128(cnt_by_3);
-				cnt_by_3 = is_divided_by(cnt_by_3, 3);
+                printf_u128(cnt_by_3);
+                cnt_by_3 = is_divided_by(cnt_by_3, 3);
 
-				if (cnt_by_3) {
-					push_stack(cnt_by_3 * 2 + 1);
-				}
-			} while (cnt_by_3);
-		}
+                if (cnt_by_3) {
+                    push_stack(cnt_by_3 * 2 + 1);
+                }
+            } while (cnt_by_3);
+        }
 
-		counter = 2 * counter + 1;
-	}
+        counter = 2 * counter + 1;
+    }
 }
 
 int main(void) {
-	odds_capacity = INITIAL_CAPACITY;
-	odds = malloc(odds_capacity * sizeof(uint128_t));
+    odds_capacity = INITIAL_CAPACITY;
+    odds = malloc(odds_capacity * sizeof(uint128_t));
 
-	stack_capacity = INITIAL_CAPACITY;
-	stack = malloc(stack_capacity * sizeof(uint128_t));
+    stack_capacity = INITIAL_CAPACITY;
+    stack = malloc(stack_capacity * sizeof(uint128_t));
 
-	push_stack(1);
+    push_stack(1);
 
-	while (stack_size > 0) {
-		main_enumeration(pop_stack());
-	}
+    while (stack_size > 0) {
+        main_enumeration(pop_stack());
+    }
 
-	free(stack);
-	free(odds);
+    free(stack);
+    free(odds);
 
-	return 0;
+    return 0;
 }
